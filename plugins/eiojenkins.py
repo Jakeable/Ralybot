6921fc0b4108290928e53856d973c1856707a503
Kamran Mackey <kamranm1200@gmail.com>
2015-04-08 06:46:52 +0100

    Made a few minor changes.

--- plugins/eiojenkins.py
+++ plugins/eiojenkins.py
@@ -2,5 +2,5 @@ from cloudbot import hook
 
 @hook.command(autohelp=False)
 def eiojenkins(text):
-    "-- Retrieves the link to the Ender IO Jenkins. ONLY USE IN #EnderIO PLEASE!"""
+"-- Retrieves the link to the Ender IO Jenkins. ONLY USE THIS IN #EnderIO PLEASE!"""
     return text + "EnderIO Jenkins - \x02http://ci.tterrag.com/job/EnderIO/\x0f"
