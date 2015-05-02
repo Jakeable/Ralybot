02222cf9f1aa33053efd94153869204984fd0b8a
Kamran Mackey <kamranm1200@gmail.com>
2015-05-02 03:38:10 -0600

    Added back the older BSD license thingy. I can't take credit for this.

--- ralybot/util/textgen.py
+++ ralybot/util/textgen.py
@@ -1,34 +1,24 @@
 """
 textgen.py
-
-A class used for quickly generating random strings.
-
+A class used to quickly generate random strings.
 Created By:
-    - Kamran Mackey <https://github.com/KamranMackey>
-
+    - Luke Rogers <https://github.com/lukeroge>
 Maintainer:
-    - Kamran Mackey <https://github.com/KamranMackey>
-
+    - Luke Rogers <https://github.com/lukeroge>
 License:
     BSD license
-
-    Copyright (c) 2013-2015 Kamran Mackey and Ralybot Contributors
+    Copyright (c) 2013-2015 Luke Rogers and CloudBot Contributors
     All rights reserved.
-
     Redistribution and use in source and binary forms, with or without modification,
     are permitted provided that the following conditions are met:
-
         1. Redistributions of source code must retain the above copyright notice,
            this list of conditions and the following disclaimer.
-
         2. Redistributions in binary form must reproduce the above copyright
            notice, this list of conditions and the following disclaimer in the
            documentation and/or other materials provided with the distribution.
-
-        3. Neither the name of Rakybot nor the names of its contributors may be used
+        3. Neither the name of CloudBot nor the names of its contributors may be used
            to endorse or promote products derived from this software without
            specific prior written permission.
-
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
